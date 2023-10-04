b07890408ce228101bdfb7730fc4fab49a968c8a
krishna-s19 <vamankrishna123@gmail.com>
2023-10-04 22:26:37 +0530

    some other changes

---
+++ MavenCmd/src/main/java/com/test/Demo.java
@@ -0,0 +1,16 @@
+package com.test;
+
+import org.testng.annotations.Test;
+
+public class Demo {
+	
+	@Test
+	public void test() {
+		System.out.println("<======>");
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("<------------>");
+	} 
+
+}
